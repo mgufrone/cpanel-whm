d26ac104ea3b53ba371ec7fb550bba26bef84d31
Mochamad Gufron <mgufronefendi@gmail.com>
2014-07-14 18:49:40 +0700

    add config and readme

---
+++ README.md
@@ -0,0 +1,44 @@
+# Laravel CPanel WHM Package
+-------------------------------
+
+## Installation Guide
+
+To install this package, you can run this code via your terminal
+	
+	composer require gufy/cpanel-whm:dev-master 
+
+Or update your `composer.json` by adding this line
+
+	"gufy/cpanel-whm":"dev-master"
+
+Then, run this code
+
+	composer update
+
+After install it, you have to add this line on your `app/config/app.php` on Service Providers lines.
+
+	'Gufy\CpanelWhm\CpanelWhmServiceProvider',
+
+And add this as an alias
+
+	'CpanelWhm'=>'Gufy\CpanelWhm\Facades\CpanelWhm',
+
+## Configuration
+
+In this package, it only using hash as its authentication. It's the safer way than using your root plain password. First, run this command
+
+	php artisan config:publish gufy/cpanel-whm
+
+It will generate new file at `app/config/packages/gufy/cpanel-whm/config.php`. Edit necessary lines.
+
+## Usage
+
+For example, if you are trying to get some list of accounts, you can run this.
+
+	<?php
+
+	Route::get('list-accounts',function(){
+		$list_accounts = CpanelWhm::listaccts();
+
+		return $list_accounts;
+	});
---
+++ src/config/config.php
@@ -0,0 +1,37 @@
+<?php 
+return [
+	/*
+	|--------------------------------------------------------------------------
+	| Host of your server
+	|--------------------------------------------------------------------------
+	|
+	| Please provide this by its full URL including its protocol and its port
+	|
+	*/
+	'host'=>'https://127.0.0.1:2087',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Remote Access key
+	|--------------------------------------------------------------------------
+	|
+	| You can find this remote access key on your CPanel/WHM server. 
+	| Log in to your server using root, and find `Remote Access Key`.
+	| Copy and paste all of the string
+	|
+	*/
+	'auth'=>'your_long_string_hash_key',
+
+	/*
+	|--------------------------------------------------------------------------
+	| Username
+	|--------------------------------------------------------------------------
+	|
+	| By default, it will use root as its username. If you have another username,
+	| make sure it has the same privelege with the root or at least it can access
+	| External API which is provided by CPanel/WHM
+	|
+	*/
+	'username'=>'root',
+
+];
