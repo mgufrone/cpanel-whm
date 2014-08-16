69b3f6f0417c016e085f501d72a398bf4fa056dd
Mochamad Gufron <mgufronefendi@gmail.com>
2014-08-17 05:18:34 +0700

    add native cpanel php as dependency

--- src/Gufy/CpanelWhm/CpanelWhm.php
+++ src/Gufy/CpanelWhm/CpanelWhm.php
@@ -1,34 +1,19 @@
 <?php namespace Gufy\CpanelWhm;
 
 use Illuminate\Support\ServiceProvider;
+use Gufy\CpanelPhp\Cpanel;
 
-class CpanelWhm {
-
-	public function __call($function, $arguments=[])
-	{
-		return $this->runQuery($function, $arguments);
-	}
+class CpanelWhm  extends Cpanel{
 
 	public function runQuery($action, $arguments)
 	{
-		$headers = [];
+		$this->setAuthorization(
+			\Config::get('cpanel-whm::username'),
+			\Config::get('cpanel-whm::auth')
+		)
+		->setHost(\Config::get('cpanel-whm::host'))
+		->setAuthType(\Config::get('cpanel-whm::auth_type', 'hash'));
 
-		$username = \Config::get('cpanel-whm::username');
-		$auth_type = \Config::get('cpanel-whm::auth_type', 'hash');
-		
-		if('hash' == $auth_type)
-			$headers['Authorization'] = 'WHM '.$username.':'. preg_replace("'(\r|\n)'","",\Config::get('cpanel-whm::auth'));
-		elseif('password' == $auth_type)
-			$headers['Authorization'] = 'Basic '.$username.':'. preg_replace("'(\r|\n)'","",\Config::get('cpanel-whm::auth'));
-
-		$host = \Config::get('cpanel-whm::host');
-		$response = \GuzzleHttp\post($host.'/json-api/'.$action, [
-		    'headers' => $headers,
-		    // 'body'    => $arguments[0],
-		    'verify'  => false,
-		    'query'	  => $arguments
-
-		]);
-		return $response->json();
+		return parent::runQuery($action, $arguments);
 	}
 }
